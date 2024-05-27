0e3f22c4c4cf5bd74c3f073f5a6f1aad68ad660a
Sarbani Roy <sarbani.roy@iws.uni-stuttgart.de>
2024-05-27 14:36:59 +0200

    select removed from authorAffiliation

--- ORCID-Vocab/update_config/get_author_orcid.js
+++ ORCID-Vocab/update_config/get_author_orcid.js
@@ -104,8 +104,8 @@ function updatePeopleInputs() {
                     var pos = item.text.search(/\d{4}-\d{4}-\d{4}-\d{3}[\dX]/);
                     if (pos >= 0) {
                         var orcid = item.text.substr(pos, 19);
-                        $(authorIdentifierSchemeSelect).val("ORCID").change();
-                        $(authorIdentifierSchemeText).text("ORCID");
+                        // $(authorIdentifierSchemeSelect).val("ORCID").change();
+                        $(authorIdentifierSchemeText).val("ORCID");
                         $(authorIdentifier).val(orcid);
 
                         if ($(authorAffiliation).val() === "") {
