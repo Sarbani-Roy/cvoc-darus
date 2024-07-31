05ff9b9c518b9cfe2a68273b726375e7091227ad
Sarbani Roy <sarbani.roy@iws.uni-stuttgart.de>
2024-07-31 13:49:55 +0200

    topic class added in console log

--- topic_vocab/get_dfg_class.js
+++ topic_vocab/get_dfg_class.js
@@ -2,4 +2,26 @@ var topicParentSelector = "div#metadata_topicClassification";
 var topicSelector = "span[data-cvoc-protocol='dfgTopicClass']";
 var topicInputSelector = "input[data-cvoc-protocol='dfgTopicClass']";
 
-console.log(topicInputSelector)
+$(document).ready(function() {
+    expandPeople();
+});
+
+function expandPeople() {
+    $(topicParentSelector).each(function() {
+        var parentElement = $(topicParentSelector).parent();
+        var fieldValuesElement = parentElement.siblings('.dataset-field-values');
+        var compoundFieldElement = fieldValuesElement.find('.edit-compound-field');
+            
+        compoundFieldElement.each(function() {
+            var topicElement = $(this);
+            console.log(topicElement)
+            if (topicElement.children().length > 2) {
+                var topicClass = topicElement.children().eq(0).find('input');
+                var topicClassVocab = topicElement.children().eq(1).find('input');
+                var topicClassVocabURI = topicElement.children().eq(3).find('input');
+
+                updatePeopleInputs(topicElement, topicClass, topicClassVocab, topicClassVocabURI);
+            }
+        });
+    });
+}
