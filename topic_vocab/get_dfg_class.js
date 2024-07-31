6f299fdf3e2b5c3b640a06dd6712b3de93a41a98
Sarbani Roy <sarbani.roy@iws.uni-stuttgart.de>
2024-07-31 13:54:35 +0200

    random changes

--- topic_vocab/get_dfg_class.js
+++ topic_vocab/get_dfg_class.js
@@ -8,20 +8,21 @@ $(document).ready(function() {
 
 function expandPeople() {
     $(topicParentSelector).each(function() {
-        var parentElement = $(topicParentSelector).parent();
-        var fieldValuesElement = parentElement.siblings('.dataset-field-values');
-        var compoundFieldElement = fieldValuesElement.find('.edit-compound-field');
+        console.log(topicParentSelector)
+        // var parentElement = $(topicParentSelector).parent();
+        // var fieldValuesElement = parentElement.siblings('.dataset-field-values');
+        // var compoundFieldElement = fieldValuesElement.find('.edit-compound-field');
             
-        compoundFieldElement.each(function() {
-            var topicElement = $(this);
-            console.log(topicElement)
-            if (topicElement.children().length > 2) {
-                var topicClass = topicElement.children().eq(0).find('input');
-                var topicClassVocab = topicElement.children().eq(1).find('input');
-                var topicClassVocabURI = topicElement.children().eq(3).find('input');
+        // compoundFieldElement.each(function() {
+        //     var topicElement = $(this);
+        //     console.log(topicElement)
+        //     if (topicElement.children().length > 2) {
+        //         var topicClass = topicElement.children().eq(0).find('input');
+        //         var topicClassVocab = topicElement.children().eq(1).find('input');
+        //         var topicClassVocabURI = topicElement.children().eq(3).find('input');
 
-                updatePeopleInputs(topicElement, topicClass, topicClassVocab, topicClassVocabURI);
-            }
-        });
+        //         updatePeopleInputs(topicElement, topicClass, topicClassVocab, topicClassVocabURI);
+        //     }
+        // });
     });
 }
