3d329d65a62102231e6e8e46e2c361968eb88393
Sarbani Roy <sarbani.roy@iws.uni-stuttgart.de>
2024-07-31 14:48:11 +0200

    js script changed

--- topic_vocab/get_dfg_classifiction.js
+++ topic_vocab/get_dfg_classifiction.js
@@ -28,97 +28,98 @@ function expandPeople() {
 function updatePeopleInputs(topicElement, topicClassValue, topicClassVocab, topicClassVocabURI) {
     $(topicElement).find(topicInputSelector).each(function() {
         var topicInput = this;
-        console.log(topicInput)
+        console.log(topicInput);
+
         if (!topicInput.hasAttribute('data-topic')) {
             // Random identifier added
             let num = Math.floor(Math.random() * 100000000000);
             $(topicInput).hide();
             $(topicInput).attr('data-topic', num);
-            
+
             // Add a select2 element to allow search and provide a list of choices
-            var selectId = "personAddSelect_" + num;
-            $(topicInput).after('<select id=' + selectId + ' class="form-control add-resource select2" tabindex="-1" aria-hidden="true">');
-            
-            $.getJSON('__dataverse_previewers__/js/dfg-2024_in_json.json', function(data) {
-                var processedResults = data.map(function(item) {
-                    return {
-                        text: item['prefLabel@en'] + " (" + item['notation'] + ")",
-                        name: item['prefLabel@en'],
-                        id: item['notation'],
-                        title: 'View more details',
-                        affiliation: item['prefLabel@en']
-                    };
-                });
+            var selectId = "topicAddSelect_" + num;
+            $(topicInput).after('<select id="' + selectId + '" class="form-control add-resource select2" tabindex="-1" aria-hidden="true"></select>');
 
-                $("#" + selectId).select2({
-                    theme: "classic",
-                    tags: $(personInput).attr('data-cvoc-allowfreetext'),
-                    delay: 500,
-                    templateResult: function(item) {
-                        
-                        console.log(item)
-                        // No templating right now
-                        if (item.loading) {
-                            return item.text;
-                        }
-                        
-                        // markMatch bolds the search term if/where it appears in the result
-                        var $result = markMatch(item.text, term);
-                        return $result;
-                    },
-                    templateSelection: function(item) {
-                        if ($(topicClassValue).val() === "" && item.name) {
-                            var topicClass = item.name;
-                        }
-                        else{
-                            var topicClass = $(topicClassValue).val();
-                        }
-                        $(topicClassVocab).val("")
-                        $(topicClassVocabURI).val("")
-
-                        item.text = topicClass;
+            $("#" + selectId).select2({
+                theme: "classic",
+                tags: $(topicInput).attr('data-cvoc-allowfreetext') === "true",
+                delay: 500,
+                templateResult: function(item) {
+                    console.log(item);
+                    if (item.loading) {
                         return item.text;
-                    },
-                    language: {
-                        searching: function(params) {
-                            return 'Search by name, email, or ORCID…';
-                        }
-                    },
-                    placeholder: personInput.hasAttribute("data-cvoc-placeholder") ? $(personInput).attr('data-cvoc-placeholder') : "Select an Author",
-                    minimumInputLength: 3,
-                    allowClear: true,
-                });
-
-                // Handle existing values
-                var topicName = $(topicInput).val()
-                if (topicName) {
-                    var newOption = new Option(topicName, topicName, true, true);
-                    $('#' + selectId).append(newOption).trigger('change');
-                }
-
-                // When a selection is made, set the value of the hidden input field
-                $('#' + selectId).on('select2:select', function(e) {
-                    var data = e.params.data;
-                    //For free-texts, the id and text are same. Otherwise different
-                    if (data.id != data.text) {
-                        var topicName = data.name;
-                        data.name = topicName;
-                        $("input[data-topic='" + num + "']").val(data.name);
-                    } else {
-                        //Tags are allowed, so just enter the text as is
-                        $("input[data-topic='" + num + "']").val(data.id);
                     }
-                });
 
-                // When a selection is cleared, clear the hidden input and all corresponding inputs
-                $('#' + selectId).on('select2:clear', function(e) {
-                    $("input[data-topic='" + num + "']").attr('value', '');
+                    // markMatch bolds the search term if/where it appears in the result
+                    var $result = markMatch(item.text, item.term);
+                    return $result;
+                },
+                templateSelection: function(item) {
+                    var topicClass = $(topicClassValue).val() === "" && item.name ? item.name : $(topicClassValue).val();
+                    $(topicClassVocab).val("");
+                    $(topicClassVocabURI).val("");
 
-                    $(topicClassVocab).val("")
-                    $(topicClassVocabURI).val("")
-                });
-                
-            });            
+                    item.text = topicClass;
+                    return item.text;
+                },
+                language: {
+                    searching: function(params) {
+                        return 'Search by name, email, or ORCID…';
+                    }
+                },
+                placeholder: topicInput.hasAttribute("data-cvoc-placeholder") ? $(topicInput).attr('data-cvoc-placeholder') : "Select an Author",
+                minimumInputLength: 3,
+                allowClear: true,
+                ajax: {
+                    transport: function(params, success, failure) {
+                        $.getJSON('__dataverse_previewers__/js/dfg-2024.json', function(data) {
+                            var processedResults = data.map(function(item) {
+                                return {
+                                    text: item['prefLabel@en'] + " (" + item['notation'] + ")",
+                                    name: item['prefLabel@en'],
+                                    id: item['notation']
+                                };
+                            });
+                            success({
+                                results: processedResults
+                            });
+                        }).fail(failure);
+                    },
+                    processResults: function(data, params) {
+                        return {
+                            results: data.results
+                        };
+                    }
+                }
+            });
+
+            // Handle existing values
+            var topicName = $(topicInput).val();
+            if (topicName) {
+                var newOption = new Option(topicName, topicName, true, true);
+                $('#' + selectId).append(newOption).trigger('change');
+            }
+
+            // When a selection is made, set the value of the hidden input field
+            $('#' + selectId).on('select2:select', function(e) {
+                var data = e.params.data;
+                // For free-texts, the id and text are the same. Otherwise, different
+                if (data.id !== data.text) {
+                    var topicName = data.name;
+                    data.name = topicName;
+                    $("input[data-topic='" + num + "']").val(data.name);
+                } else {
+                    // Tags are allowed, so just enter the text as is
+                    $("input[data-topic='" + num + "']").val(data.id);
+                }
+            });
+
+            // When a selection is cleared, clear the hidden input and all corresponding inputs
+            $('#' + selectId).on('select2:clear', function(e) {
+                $("input[data-topic='" + num + "']").val('');
+                $(topicClassVocab).val("");
+                $(topicClassVocabURI).val("");
+            });
         }
     });
 }
