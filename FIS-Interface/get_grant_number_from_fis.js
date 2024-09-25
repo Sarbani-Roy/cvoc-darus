fc69bdcbd3c41567802c1288fcdc9888c39f54c8
Sarbani-Roy <98636981+Sarbani-Roy@users.noreply.github.com>
2024-09-25 10:27:26 +0200

    trying to debug '-'

--- FIS-Interface/get_grant_number_from_fis.js
+++ FIS-Interface/get_grant_number_from_fis.js
@@ -250,15 +250,17 @@ function updateGrantInputs(projectElement, projectNameInput, projectAcronymInput
 
                             console.log($(clearFundingAgency).val());
                             console.log($(clearProjectGrantAcronymInput).val());
-                            console.log(clearFundingElement.siblings('.field-add-delete'));
-                            console.log(clearFundingElement.siblings('.field-add-delete').children().eq(1));
-
-                            if (clearFundingElement.siblings('.field-add-delete').children().length > 1) {
-                                clearFundingElement.siblings('.field-add-delete').children().eq(1).click();
-                            }
-
-                            console.log($(clearFundingAgency).val());
-                            console.log($(clearProjectGrantAcronymInput).val());
+                            setTimeout(function() {
+                                console.log(clearFundingElement.siblings('.field-add-delete').children().length);
+                                console.log(clearFundingElement.siblings('.field-add-delete').children().eq(1));
+            
+                                if (clearFundingElement.siblings('.field-add-delete').children().length > 1) {
+                                    clearFundingElement.siblings('.field-add-delete').children().eq(1).click();
+                                }
+            
+                                console.log($(clearFundingAgency).val());
+                                console.log($(clearProjectGrantAcronymInput).val());
+                            }, 1000);
                         }
                     });
                 });
