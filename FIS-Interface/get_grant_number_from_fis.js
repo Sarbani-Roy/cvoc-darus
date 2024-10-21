7b3de86f522c62e4032d88c46bd8e0bb03fc2c31
Sarbani-Roy <98636981+Sarbani-Roy@users.noreply.github.com>
2024-10-21 11:27:56 +0200

    some more console.log added

--- FIS-Interface/get_grant_number_from_fis.js
+++ FIS-Interface/get_grant_number_from_fis.js
@@ -249,13 +249,14 @@ function updateGrantInputs(projectElement, projectNameInput, projectAcronymInput
 
             // Detect when the select2 dropdown is opened (user interaction)
             $("#" + selectId).on('select2:open', function(e) {
-                console.log("Processed item in open", processedItemsSet);
                 previousAcronym = $(projectAcronymInput).val();
                 processedItemsSet.clear();
+                console.log("Processed item in open", processedItemsSet);
             });
             
             // When a selection is made, set the value of the hidden input field
             $('#' + selectId).on('select2:select', function(e) {
+                processedItemsSet.clear();
                 console.log("Processed item in select", processedItemsSet);
                 var data = e.params.data;         
                 var newAcronym = data.acronym;
