14484d885d1b66f6f3fb10218439979aad0d9431
Sarbani-Roy <98636981+Sarbani-Roy@users.noreply.github.com>
2024-10-29 15:59:47 +0100

    random try to trigger change

--- FIS-Interface/get_grant_number_from_fis.js
+++ FIS-Interface/get_grant_number_from_fis.js
@@ -128,6 +128,7 @@ function updateGrantInputs(projectElement, projectNameInput, projectAcronymInput
                         if (item.id && item.text != item.id) {
                             $(fisIdentifierInput).val(item.id);
                         }
+                        $(projectAcronymInput).trigger('change');
                         $(projectLevelInput).val('');                                                      
                         if (item.text) {
                             var projectName = item.text;
